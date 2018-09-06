4cf851b054f709bbd8b810eee38ffb941b1946ba
Angel Fernando Sepulveda Lozada <37251039+1430382@users.noreply.github.com>
2018-09-06 16:03:29 -0500

    Update test.c

--- test.c
+++ test.c
@@ -1,26 +1,55 @@
 #include<stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define for_loop(start, end, incr) for(i = start; \
+                                        i < end;  \
+                                         i += incr)
+
 int main(int argc,char* argv[])
 {
-    int counter;
-    const char *k;
-    const char *t;
-    printf("Program Name Is: %s",argv[0]);
-    if(argc==1)
-        printf("\nNo Extra Command Line Argument Passed Other Than Program Name");
-    if(argc>=2)
-    {
-        printf("\nNumber Of Arguments Passed: %d",argc);
-        printf("\n----]Arguments----");
-        for(counter=0;counter<argc;counter++)
-           // printf("\nargv[%d]: %s",counter,argv[counter]);
-	    
-	    k=argv[counter];
-	    printf("\n%s",k);
-	    printf("\n");
-	    t=argv[counter];
-	    printf("\n%s",t);
-	    printf("\n");
-	    
-    }
+	int counter,b,k,t,arr[t], i=0;
+	char *p,*p1;	
+	printf("Program Name Is: %s\n",argv[0]);
+	if(argc>=2)
+	{
+		printf("\nNumber Of Arguments Passed: %d",argc);
+		printf("\n----Arguments----\n");
+        	//for(counter=0;counter<argc;counter++)
+           	// printf("\nargv[%d]: %s",counter,argv[counter]);
+	}
+	if(strcmp("-t", argv[1]))
+		{
+		printf("The first parameter should be -t\n");
+		}
+	else{
+		long conv = strtol(argv[2], &p, 10);
+		t=conv;
+		printf("Value of t :%d",t);
+		printf("\n");
+	}
+	if(strcmp("-k", argv[3]))
+		{
+		printf("The second parameter should be -k\n");
+	}
+	else{
+		long conv1 = strtol(argv[4], &p1, 10);
+		k = conv1;
+		printf("Value of k :%d",k);
+		printf("\n");			
+	}
+	for_loop(i, t, 1)
+	{
+		*p=i;	
+		printf("%d",*p);
+		printf("\n");
+	}
+	
+	printf("\n");
+	b=1;
+	for_loop(1,t,1)
+	{
+		b=(b*(k-i+1))/i;
+	}
+	printf("result %d",b);
     return 0;
 }
